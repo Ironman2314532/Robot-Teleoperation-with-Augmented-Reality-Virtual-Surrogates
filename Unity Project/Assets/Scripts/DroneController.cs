741ca84d2b68ef5f2b6caa827d8e59733dbb31be
Parth Patel <parth.pmech@gmail.com>
2022-11-06 20:31:58 -0500

    Add Drone Transition States

--- Unity Project/Assets/Scripts/DroneController.cs
+++ Unity Project/Assets/Scripts/DroneController.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
+    enum DroneState
+    {
+        DRONE_STATE_IDLE,
+        DRONE_STATE_START_TAKINGOFF,
+        DRONE_STATE_TAKINGOFF,
+        DRONE_STATE_MOVING_UP,
+        DRONE_STATE_FLYING,
+    }
+
+    DroneState _State;
     Animator _Anim;
     Vector3 _Speed = new Vector3(0.0f, 0.0f, 0.0f);
     public float _SpeedMultipler = 1.0f;
+
+    public bool IsIdle()
+    {
+        return (_State == DroneState.DRONE_STATE_IDLE);
+    }
+
+    public void TakeOff()
+    {
+        _State = DroneState.DRONE_STATE_START_TAKINGOFF;
+    }
+
     public void Move(float _speedX, float _speedZ)
     {
         _Speed.x = _speedX;
@@ -14,19 +35,38 @@ public class DroneController : MonoBehaviour
         UpdateDrone();
     }
 
-
     void UpdateDrone()
     {
-        float angleZ = -30.0f * _Speed.x * 60.0f * Time.deltaTime;
-        float angleX = 30.0f * _Speed.z * 60.0f * Time.deltaTime;
-        Vector3 rotation = transform.localRotation.eulerAngles;
-        transform.localPosition += _Speed * _SpeedMultipler * Time.deltaTime;
-        transform.localRotation = Quaternion.Euler(angleX, rotation.y, angleZ);
+        switch (_State)
+        {
+            case DroneState.DRONE_STATE_IDLE:
+                break;
+            case DroneState.DRONE_STATE_START_TAKINGOFF:
+                _Anim.SetBool("TakeOff", true);
+                _State = DroneState.DRONE_STATE_TAKINGOFF;
+                break;
+            case DroneState.DRONE_STATE_TAKINGOFF:
+                if (_Anim.GetBool("TakeOff") == false)
+                    _State = DroneState.DRONE_STATE_MOVING_UP;
+                break;
+            case DroneState.DRONE_STATE_MOVING_UP:
+                if (_Anim.GetBool("MoveUp") == false)
+                    _State = DroneState.DRONE_STATE_FLYING;
+                break;
+            case DroneState.DRONE_STATE_FLYING:
+                float angleZ = -30.0f * _Speed.x * 60.0f * Time.deltaTime;
+                float angleX = 30.0f * _Speed.z * 60.0f * Time.deltaTime;
+                Vector3 rotation = transform.localRotation.eulerAngles;
+                transform.localPosition += _Speed * _SpeedMultipler * Time.deltaTime;
+                transform.localRotation = Quaternion.Euler(angleX, rotation.y, angleZ);
+                break;
+        }
+        Debug.Log(_State);
     }
     void Start()
     {
         _Anim = GetComponent<Animator>();
-        _Anim.SetBool("TakeOff", true);
+        _State = DroneState.DRONE_STATE_IDLE;
     }
 
     void Update()
--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public DroneController _DroneController;
+    public Button _FlyButton;
 
+    void EventOnClickFlyButton()
+    {
+        if (_DroneController.IsIdle())
+            _DroneController.TakeOff();
+    }
+
+    void Start()
+    {
+        _FlyButton.onClick.AddListener(EventOnClickFlyButton);
+    }
 
     void Update()
     {
