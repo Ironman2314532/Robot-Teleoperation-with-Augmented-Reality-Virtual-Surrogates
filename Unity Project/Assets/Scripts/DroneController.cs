59fc31a51ff14c238487c7b9fb936b02066dfb19
Parth Patel <parth.pmech@gmail.com>
2022-11-06 21:22:12 -0500

    Code Drone Landing

--- Unity Project/Assets/Scripts/DroneController.cs
+++ Unity Project/Assets/Scripts/DroneController.cs
@@ -11,6 +11,10 @@ public class DroneController : MonoBehaviour
         DRONE_STATE_TAKINGOFF,
         DRONE_STATE_MOVING_UP,
         DRONE_STATE_FLYING,
+        DRONE_STATE_START_LANDING,
+        DRONE_STATE_LANDING,
+        DRONE_STATE_LANDED,
+        DRONE_STATE_WAIT_ENGINE_STOP
     }
 
     DroneState _State;
@@ -26,6 +30,18 @@ public class DroneController : MonoBehaviour
     public void TakeOff()
     {
         _State = DroneState.DRONE_STATE_START_TAKINGOFF;
+    }
+
+    public bool IsFlying()
+    {
+        return (_State == DroneState.DRONE_STATE_FLYING);
+
+    }
+
+    public void Land()
+    {
+        _State = DroneState.DRONE_STATE_START_LANDING;
+
     }
 
     public void Move(float _speedX, float _speedZ)
@@ -59,6 +75,23 @@ public class DroneController : MonoBehaviour
                 Vector3 rotation = transform.localRotation.eulerAngles;
                 transform.localPosition += _Speed * _SpeedMultipler * Time.deltaTime;
                 transform.localRotation = Quaternion.Euler(angleX, rotation.y, angleZ);
+                break;
+            case DroneState.DRONE_STATE_START_LANDING:
+                _Anim.SetBool("MoveDown", true);
+                Debug.Log(_State);
+                _State = DroneState.DRONE_STATE_LANDING;
+                break;
+            case DroneState.DRONE_STATE_LANDING:
+                if (_Anim.GetBool("MoveDown") == false)
+                    _State = DroneState.DRONE_STATE_LANDED;
+                break;
+            case DroneState.DRONE_STATE_LANDED:
+                _Anim.SetBool("Land", true);
+                _State = DroneState.DRONE_STATE_WAIT_ENGINE_STOP;
+                break;
+            case DroneState.DRONE_STATE_WAIT_ENGINE_STOP:
+                if (_Anim.GetBool("Land") == false)
+                    _State = DroneState.DRONE_STATE_IDLE;
                 break;
         }
         Debug.Log(_State);
--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -7,23 +7,42 @@ public class GameManager : MonoBehaviour
 {
     public DroneController _DroneController;
     public Button _FlyButton;
+    public Button _LandButton;
 
     void EventOnClickFlyButton()
     {
         if (_DroneController.IsIdle())
+        {
             _DroneController.TakeOff();
+            _FlyButton.gameObject.SetActive(false);
+        }
+    }
+
+    void EventOnClickLandButton()
+    {
+        if (_DroneController.IsFlying())
+        {
+            _DroneController.Land();
+            _FlyButton.gameObject.SetActive(true);
+        }
     }
 
     void Start()
     {
         _FlyButton.onClick.AddListener(EventOnClickFlyButton);
+        _LandButton.onClick.AddListener(EventOnClickLandButton);
+        _LandButton.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        if (_DroneController.IsFlying())
+        {
+            _LandButton.gameObject.SetActive(true);
+            Debug.Log("Activating Land Button");
+        }
         float speedX = Input.GetAxis("Horizontal");
         float speedZ = Input.GetAxis("Vertical");
-
         _DroneController.Move(speedX, speedZ);
     }
 }
