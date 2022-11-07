008486735a4aff83da30fc1c037c9a7a5ed9e54f
Parth Patel <parth.pmech@gmail.com>
2022-11-06 22:22:30 -0500

    Add GamePad Control as inputs

--- Unity Project/Assets/Scripts/GameManager.cs
+++ Unity Project/Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public DroneController _DroneController;
+    ControllerInput controls;
     public Button _FlyButton;
     public Button _LandButton;
+    bool _drone_power_state = false;
+    Vector2 translate_drone;
 
     void EventOnClickFlyButton()
     {
@@ -15,6 +19,7 @@ public class GameManager : MonoBehaviour
         {
             _DroneController.TakeOff();
             _FlyButton.gameObject.SetActive(false);
+            _drone_power_state = true;
         }
     }
 
@@ -23,8 +28,39 @@ public class GameManager : MonoBehaviour
         if (_DroneController.IsFlying())
         {
             _DroneController.Land();
-            _FlyButton.gameObject.SetActive(true);
+            _LandButton.gameObject.SetActive(false);
+            _drone_power_state = false;
         }
+    }
+
+    void power_switch_pressed()
+    {
+        _drone_power_state = !_drone_power_state;
+        if (_drone_power_state)
+            EventOnClickFlyButton();
+        else
+            EventOnClickLandButton();
+    }
+
+
+    void Awake()
+    {
+        controls = new ControllerInput();
+        controls.Power.DronePowerSwitch.performed += ctx => power_switch_pressed();
+        controls.Translate.Move.performed += ctx => translate_drone = ctx.ReadValue<Vector2>();
+        controls.Translate.Move.canceled += CollectionExtensions => translate_drone = Vector2.zero;
+    }
+
+    void OnEnable()
+    {
+        controls.Power.Enable();
+        controls.Translate.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Power.Disable();
+        controls.Translate.Disable();
     }
 
     void Start()
@@ -32,17 +68,15 @@ public class GameManager : MonoBehaviour
         _FlyButton.onClick.AddListener(EventOnClickFlyButton);
         _LandButton.onClick.AddListener(EventOnClickLandButton);
         _LandButton.gameObject.SetActive(false);
+        _FlyButton.gameObject.SetActive(false);
     }
 
     void Update()
     {
         if (_DroneController.IsFlying())
-        {
             _LandButton.gameObject.SetActive(true);
-            Debug.Log("Activating Land Button");
-        }
-        float speedX = Input.GetAxis("Horizontal");
-        float speedZ = Input.GetAxis("Vertical");
-        _DroneController.Move(speedX, speedZ);
+        if (_DroneController.IsIdle())
+            _FlyButton.gameObject.SetActive(true);
+        _DroneController.Move(translate_drone.x, translate_drone.y);
     }
 }
